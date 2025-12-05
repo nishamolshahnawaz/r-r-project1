5b60fb0ecce7fd6e02867cc7a7d51496166c975f
Nishamol Shahnawaz <nishamol.nishan@gmail.com>
2025-12-06 00:56:56 +0400

    Create name_split.py
    Created function for split the full name

---
+++ name_split.py
@@ -0,0 +1,6 @@
+str2=input("Enter your full name:")
+# name_sp=str2.split(" ")
+# f_name=name_sp[0]
+# s_name=name_sp[1]
+# print(f"original order:{str2} ")
+# print(f"Reverse order:{s_name} {f_name} ")
